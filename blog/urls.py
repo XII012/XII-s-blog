d38b2291c31358a446e78cccbba5e3f449ef7e86
XII012 <zkw374628354@gmail.com>
2020-10-13 17:30:55 +0800

    write post_deatil

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     #转移url请求到views
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>', views.post_detail, name='post_detail'),
 ]
